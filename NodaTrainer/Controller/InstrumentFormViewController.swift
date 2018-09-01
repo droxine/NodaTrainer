971434307b344729497e41e41b174909f9ce7efc
Walter Marcelo <walter12_4@hotmail.com>
2018-09-01 17:26:35 -0500

    Both forms: Music Classes and Instruments

--- NodaTrainer/Controller/InstrumentFormViewController.swift
+++ NodaTrainer/Controller/InstrumentFormViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class InstrumentFormViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtStock: UITextField!
+    @IBOutlet weak var txtPrice: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtDiscount: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    @IBOutlet weak var txtComments: UITextView!
+    
+    var indexState: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +30,28 @@ class InstrumentFormViewController: UIViewController {
     }
     
     @IBAction func addInstrument(_ sender: Any) {
+        //guardar Firebase
+        
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtName.resignFirstResponder()
+        txtStock.resignFirstResponder()
+        txtPrice.resignFirstResponder()
+        txtPhone.resignFirstResponder()
+        txtDiscount.resignFirstResponder()
+        txtDescription.resignFirstResponder()
+        txtComments.resignFirstResponder()
+    }
+    
+    @IBAction func switchState(_ sender: UISegmentedControl) {
+        indexState = sender.selectedSegmentIndex
+    }
+    
     
 }
--- NodaTrainer/Controller/MusicClassFormViewController.swift
+++ NodaTrainer/Controller/MusicClassFormViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class MusicClassFormViewController: UIViewController {
 
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtPrice: UITextField!
+    @IBOutlet weak var txtProfessor: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    @IBOutlet weak var txtComments: UITextView!
     
+    var indexType: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +38,17 @@ class MusicClassFormViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func switchType(_ sender: UISegmentedControl) {
+        indexType = sender.selectedSegmentIndex
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtTitle.resignFirstResponder()
+        txtPrice.resignFirstResponder()
+        txtProfessor.resignFirstResponder()
+        txtPhone.resignFirstResponder()
+        txtDescription.resignFirstResponder()
+        txtComments.resignFirstResponder()
+    }
 
 }
