cacef20b29e3f56c0b2d03a164b56542322beebf
Walter Marcelo <walter12_4@hotmail.com>
2018-09-04 01:05:01 -0500

    Lesson 0 finished.

--- NodaTrainer/Controller/Lecciones/Lection0Continue1ViewController.swift
+++ NodaTrainer/Controller/Lecciones/Lection0Continue1ViewController.swift
@@ -1,16 +1,15 @@
 //
-//  Lection0ViewController.swift
+//  Lection0Continue1ViewController.swift
 //  NodaTrainer
 //
-//  Created by sangeles on 9/2/18.
+//  Created by sangeles on 9/3/18.
 //  Copyright © 2018 SAM Creators. All rights reserved.
 //
 
 import UIKit
 
-class Lection0ViewController: UIViewController {
-    
-    
+class Lection0Continue1ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,7 +24,7 @@ class Lection0ViewController: UIViewController {
     }
     
     @IBAction func goNext(_ sender: Any) {
-        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "lection0Comtinue1") as! Lection0Continue1ViewController
+        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "lection0Comtinue2") as! Lection0Continue2ViewController
         present(controllerTravel, animated: true, completion: nil)
     }
     
---
+++ NodaTrainer/Controller/Lecciones/Lection0Continue2ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  Lection0Continue2ViewController.swift
+//  NodaTrainer
+//
+//  Created by sangeles on 9/4/18.
+//  Copyright © 2018 SAM Creators. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class Lection0Continue2ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playNotes(_ sender: Any) {
+        let sound = Bundle.main.url(forResource:"0001notas", withExtension: "mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: sound!)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+    
+    
+    @IBAction func goLessons(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
