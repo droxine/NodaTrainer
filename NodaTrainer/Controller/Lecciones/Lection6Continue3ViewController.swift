2f25308800f9f5ccf834f70b86d18a7346114490
Walter Marcelo <walter12_4@hotmail.com>
2018-09-27 11:06:19 -0500

    Lesson 6 Completed.

--- NodaTrainer/Controller/Lecciones/Lection6Continue3ViewController.swift
+++ NodaTrainer/Controller/Lecciones/Lection6Continue3ViewController.swift
@@ -1,4 +1,4 @@
-//  Lection6Continue2ViewController.swift
+//  Lection6Continue3ViewController.swift
 //  NodaTrainer
 //
 //  Created by sangeles on 9/27/18.
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import Firebase
 import AVFoundation
 
-class Lection6Continue2ViewController: UIViewController {
+class Lection6Continue3ViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
     
@@ -24,7 +25,7 @@ class Lection6Continue2ViewController: UIViewController {
     @IBOutlet weak var labelResult2: UILabel!
     
     @IBOutlet weak var btnReload: UIButton!
-    @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var btnFinish: UIButton!
     var notesPressed: Array<String> = []
 
     override func viewDidLoad() {
@@ -43,7 +44,7 @@ class Lection6Continue2ViewController: UIViewController {
         labelResult.isHidden = true
         labelResult2.isHidden = true
         btnReload.isHidden = true
-        btnNext.isEnabled = false
+        btnFinish.isEnabled = false
         notesPressed.removeAll()
         btnSol.backgroundColor = UIColor.white
         btnLa.backgroundColor = UIColor.white
@@ -56,7 +57,7 @@ class Lection6Continue2ViewController: UIViewController {
     }
     
     @IBAction func playSound(_ sender: Any) {
-        let sound = Bundle.main.url(forResource:"0009 Do, re,  mi, fa, sol, la, 3", withExtension: "mp3")
+        let sound = Bundle.main.url(forResource:"0009 Do, re,  mi, fa, sol, la, 4", withExtension: "mp3")
         reproduceSound(sound!)
     }
     
@@ -137,11 +138,11 @@ class Lection6Continue2ViewController: UIViewController {
         labelResult.isHidden = false
         labelResult2.isHidden = false
         btnReload.isHidden = false
-        btnNext.isEnabled = true
+        btnFinish.isEnabled = true
         btnSol.backgroundColor = UIColor.green
         btnLa.backgroundColor = UIColor.green
         btnTi.backgroundColor = UIColor.green
-        let result = "SiLaSolSi"
+        let result = "SiSolLaSi"
         var answer: String = ""
         for note in notesPressed {
             answer.append(note)
@@ -160,7 +161,7 @@ class Lection6Continue2ViewController: UIViewController {
         labelResult.isHidden = true
         labelResult2.isHidden = true
         btnReload.isHidden = true
-        btnNext.isEnabled = true
+        btnFinish.isEnabled = true
         btnSol.backgroundColor = UIColor.white
         btnLa.backgroundColor = UIColor.white
         btnTi.backgroundColor = UIColor.white
@@ -200,13 +201,28 @@ class Lection6Continue2ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil);
     }
     
-    @IBAction func goBack(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    @IBAction func completeLesson(_ sender: Any) {
+        saveLessonsDone() { success in
+            if !success {
+                print("Error: No se pudo actualizar el fin de la leccion")
+            }
+        }
+        
+        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
+        controllerTravel.selectedIndex = 1
+        present(controllerTravel, animated: true, completion: nil)
     }
     
-    @IBAction func goNext(_ sender: Any) {
-        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "lection6Continue3") as! Lection6Continue3ViewController
-        present(controllerTravel, animated: true, completion: nil)
+    func saveLessonsDone(completion: @escaping ((_ success: Bool) -> ()) ) -> Void{
+        guard let uid = Auth.auth().currentUser?.uid else { return}
+        print(uid)
+        let lessonsObject = [
+            "done": true
+            ] as [String:Any]
+        let childUpdates = ["/lessons/\(uid)/": lessonsObject]
+        Database.database().reference().updateChildValues(childUpdates, withCompletionBlock: { (error, ref) in
+            completion(error == nil)
+        })
     }
     
 
