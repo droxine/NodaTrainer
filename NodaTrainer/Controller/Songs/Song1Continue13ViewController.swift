1efc82fb9e1fb46d7d843789565d481df444c448
Walter Marcelo <walter12_4@hotmail.com>
2018-09-26 19:06:35 -0500

    Song 1 Part 13 Added.

--- NodaTrainer/Controller/Songs/Song1Continue13ViewController.swift
+++ NodaTrainer/Controller/Songs/Song1Continue13ViewController.swift
@@ -1,4 +1,4 @@
-//  Song1Continue12ViewController.swift
+//  Song1Continue13ViewController.swift
 //  NodaTrainer
 //
 //  Created by sangeles on 9/26/18.
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-class Song1Continue12ViewController: UIViewController {
+class Song1Continue13ViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
     
@@ -45,8 +45,8 @@ class Song1Continue12ViewController: UIViewController {
         btnReload.isHidden = true
         btnNext.isEnabled = false
         notesPressed.removeAll()
-        btnMi.backgroundColor = UIColor.white
-        btnSol.backgroundColor = UIColor.white
+        btnFa.backgroundColor = UIColor.white
+        btnRe.backgroundColor = UIColor.white
     }
     
     func setBorder(_ button: UIButton) {
@@ -55,7 +55,7 @@ class Song1Continue12ViewController: UIViewController {
     }
     
     @IBAction func playSound(_ sender: Any) {
-        let sound = Bundle.main.url(forResource:"0007 Remando suavemente 13", withExtension: "mp3")
+        let sound = Bundle.main.url(forResource:"0007 Remando suavemente 14", withExtension: "mp3")
         reproduceSound(sound!)
     }
     
@@ -149,9 +149,9 @@ class Song1Continue12ViewController: UIViewController {
         labelResult2.isHidden = false
         btnReload.isHidden = false
         btnNext.isEnabled = true
-        btnMi.backgroundColor = UIColor.green
-        btnSol.backgroundColor = UIColor.green
-        let result = "SolMiMiMi"
+        btnFa.backgroundColor = UIColor.green
+        btnRe.backgroundColor = UIColor.green
+        let result = "FaReReRe"
         var answer: String = ""
         for note in notesPressed {
             answer.append(note)
@@ -171,8 +171,8 @@ class Song1Continue12ViewController: UIViewController {
         labelResult2.isHidden = true
         btnReload.isHidden = true
         btnNext.isEnabled = true
-        btnMi.backgroundColor = UIColor.white
-        btnSol.backgroundColor = UIColor.white
+        btnFa.backgroundColor = UIColor.white
+        btnRe.backgroundColor = UIColor.white
     }
     
     //Alert message. Receives the message as a parameter
@@ -202,9 +202,8 @@ class Song1Continue12ViewController: UIViewController {
     }
     
     @IBAction func goNext(_ sender: Any) {
-        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "song1Continue13") as! Song1Continue13ViewController
-        present(controllerTravel, animated: true, completion: nil)
+        
     }
     
-    
+
 }
