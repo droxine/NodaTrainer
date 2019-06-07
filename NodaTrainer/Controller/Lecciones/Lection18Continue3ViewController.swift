3fbe905c112350c977cba027af5654b7840f97c9
sangeles <stampaa@outlook.com>
2019-06-07 07:06:38 -0500

    Lesson 18 completed.

---
+++ NodaTrainer/Controller/Lecciones/Lection18Continue3ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  Lection18Continue3ViewController.swift
+//  NodaTrainer
+//
+//  Created by sangeles on 6/7/19.
+//  Copyright © 2019 SAM Creators. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Firebase
+
+class Lection18Continue3ViewController: UIViewController {
+
+    var audioPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func reproduceSound(_ resource: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: resource)
+        } catch {
+            print(error)
+        }
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        audioPlayer.play()
+    }
+    
+    @IBAction func playSound(_ sender: Any) {
+        let sound = Bundle.main.url(forResource:"2da menor ascendente", withExtension: "mp3")
+        reproduceSound(sound!)
+    }
+    
+    @IBAction func checkSegundaMenor(_ sender: Any) {
+        showMessage(correct: true)
+    }
+    
+    @IBAction func checkTerceraMenor(_ sender: Any) {
+        showMessage(correct: false)
+    }
+    
+    func showMessage(correct:Bool) {
+        if(correct) {
+            displayAlertMessage(message: "Felicitaciones. Acertó en todo.")
+        } else {
+            displayAlertMessage(message: "Incorrecto. Debe practicar más")
+        }
+    }
+    
+    func displayAlertMessage(message:String) {
+        let alert = UIAlertController(title: "Resultado", message: message, preferredStyle: UIAlertController.Style.alert);
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil);
+        
+        alert.addAction(okAction);
+        
+        self.present(alert, animated: true, completion: nil);
+    }
+    
+    @IBAction func completeLesson(_ sender: Any) {
+        saveLessonsDone() { success in
+            if !success {
+                print("Error: No se pudo actualizar el fin de la leccion")
+            }
+        }
+        
+        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
+        controllerTravel.selectedIndex = 1
+        present(controllerTravel, animated: true, completion: nil)
+    }
+    
+    func saveLessonsDone(completion: @escaping ((_ success: Bool) -> ()) ) -> Void{
+        guard let uid = Auth.auth().currentUser?.uid else { return}
+        print(uid)
+        let lessonsObject = [
+            "done": true
+            ] as [String:Any]
+        let childUpdates = ["/lessons/\(uid)/": lessonsObject]
+        Database.database().reference().updateChildValues(childUpdates, withCompletionBlock: { (error, ref) in
+            completion(error == nil)
+        })
+    }
+
+}
---
+++ NodaTrainer/Controller/Lecciones/Lection18ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  Lection18ViewController.swift
+//  NodaTrainer
+//
+//  Created by sangeles on 6/7/19.
+//  Copyright © 2019 SAM Creators. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class Lection18ViewController: UIViewController {
+
+    var audioPlayer: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func reproduceSound(_ resource: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: resource)
+        } catch {
+            print(error)
+        }
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        audioPlayer.play()
+    }
+    
+    @IBAction func playSound(_ sender: Any) {
+        let sound = Bundle.main.url(forResource:"3ra menor descendente", withExtension: "mp3")
+        reproduceSound(sound!)
+    }
+    
+    @IBAction func checkSegundaMenor(_ sender: Any) {
+        showMessage(correct: false)
+    }
+    
+    @IBAction func checkTerceraMenor(_ sender: Any) {
+        showMessage(correct: true)
+    }
+    
+    func showMessage(correct:Bool) {
+        if(correct) {
+            displayAlertMessage(message: "Felicitaciones. Acertó en todo.")
+        } else {
+            displayAlertMessage(message: "Incorrecto. Debe practicar más")
+        }
+    }
+    
+    func displayAlertMessage(message:String) {
+        let alert = UIAlertController(title: "Resultado", message: message, preferredStyle: UIAlertController.Style.alert);
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil);
+        
+        alert.addAction(okAction);
+        
+        self.present(alert, animated: true, completion: nil);
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func goNext(_ sender: Any) {
+        let controllerTravel = self.storyboard?.instantiateViewController(withIdentifier: "lection18Continue1") as! Lection18Continue1ViewController
+        present(controllerTravel, animated: true, completion: nil)
+    }
+
+}
