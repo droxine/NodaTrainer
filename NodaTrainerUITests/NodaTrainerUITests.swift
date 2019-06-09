4dcf01b6b4aa2fad1f88093ca34d99924395d2f8
sangeles <stampaa@outlook.com>
2019-06-09 17:12:26 -0500

    Prueba automatizada de la lección 21

--- NodaTrainerUITests/NodaTrainerUITests.swift
+++ NodaTrainerUITests/NodaTrainerUITests.swift
@@ -2,7 +2,7 @@
 //  NodaTrainerUITests.swift
 //  NodaTrainerUITests
 //
-//  Created by walter on 4/15/18.
+//  Created by sangeles on 4/15/18.
 //  Copyright © 2018 SAM Creators. All rights reserved.
 //
 
@@ -12,25 +12,55 @@ class NodaTrainerUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLection21() {
+        
+        let app = XCUIApplication()
+        app.tabBars.children(matching: .button).element(boundBy: 1).tap()
+        
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.otherElements.containing(.button, identifier:"MENUIMG HOMECLICKED").element.tap()
+        scrollViewsQuery.children(matching: .image).matching(identifier: "avatar").element(boundBy: 9).tap()
+        
+        let playButton = app.buttons["PLAY"]
+        playButton.tap()
+        app.buttons["Segunda Mayor"].tap()
+        
+        let okButton = app.alerts["Resultado"].buttons["OK"]
+        okButton.tap()
+        
+        let siguienteButton = app.buttons["SIGUIENTE"]
+        siguienteButton.tap()
+        playButton.tap()
+        app.buttons["Segunda Menor"].tap()
+        okButton.tap()
+        siguienteButton.tap()
+        playButton.tap()
+        
+        let terceraMayorButton = app.buttons["Tercera Mayor"]
+        terceraMayorButton.tap()
+        okButton.tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element(boundBy: 2).children(matching: .other).element.tap()
+        siguienteButton.tap()
+        playButton.tap()
+        terceraMayorButton.tap()
+        okButton.tap()
+        siguienteButton.tap()
+        playButton.tap()
+        terceraMayorButton.tap()
+        okButton.tap()
+        siguienteButton.tap()
+        playButton.tap()
+        app.buttons["Tercera Menor"].tap()
+        okButton.tap()
+        app.buttons["COMPLETAR LECCIÓN"].tap()
     }
     
 }
