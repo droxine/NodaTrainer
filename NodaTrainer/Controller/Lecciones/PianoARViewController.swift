75859747fd6d1891f4553b9ae6e57db8ee4a0b82
sangeles <stampaa@outlook.com>
2019-07-17 01:11:59 -0500

    Audio added to the piano AR, and added Camerata logo.

--- NodaTrainer/Controller/Lecciones/PianoARViewController.swift
+++ NodaTrainer/Controller/Lecciones/PianoARViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import SceneKit
 import ARKit
+import AVFoundation
 
 class PianoARViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var pianoView: ARSCNView!
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +22,8 @@ class PianoARViewController: UIViewController, ARSCNViewDelegate {
         pianoView.delegate = self
         let scene = SCNScene(named: "SceneKit Piano.scnassets/Piano.scn")
         if let node = scene?.rootNode.childNode(withName: "Cube_002", recursively: true) {
-            node.position = SCNVector3(0, 0, -1.6)
+            node.position = SCNVector3(0, 0, -1.4)
+            node.scale = .init(0.80, 0.80, 0.80)
             pianoView.scene.rootNode.addChildNode(node)
         }
         
@@ -45,10 +48,32 @@ class PianoARViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let results = pianoView.hitTest(touch.location(in: pianoView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitFeature = results.last else { return }
+        let notes = ["Do","Re","Mi","Fa","Sol","La","Si",
+                     "Do2","Re2","Mi2","Fa2","Sol2","La2","Si2","Do5"]
+        let randomNote : Int = Int(arc4random_uniform(15))
+        let sound = Bundle.main.url(forResource:notes[randomNote], withExtension: "mp3")
+        reproduceSound(sound!)
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         pianoView.session.pause()
     }
     
+    func reproduceSound(_ resource: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: resource)
+        } catch {
+            print(error)
+        }
+        if audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        audioPlayer.play()
+    }
 
 }
